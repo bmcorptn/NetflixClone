26de48fe870624329d25c62581c850c3340cb830
bmcorptn <bmcorptn@gmail.com>
2020-08-23 08:31:02 +0200

    deployed on FireBase...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import requests from './requests';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My own NetFlix Clone...</h1>
-      <Row title="NETFLIX ORIGINaLS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+    <div className="app">
+      {/*Nav*/}
+      <Nav />
+      {/*Banner*/}
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Animation Movies" fetchUrl={requests.fetchAnimationMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,31 +1,63 @@
-import React, {useState, useEffect} from 'react'
-import axios from './axios'
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const baseUrl = "https://image.tmdb.org/t/p/original/";
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-    const [movies, setMovies] = useState([])
-    const baseUrl="https://image.tmdb.org/t/p/original/"
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      //console.table(request.data.results);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-    useEffect(() => {
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            console.table(request.data.results)
-            setMovies(request.data.results)
-            return request;
-        }
-        fetchData()
-    }, [fetchUrl])
-
-    return (
-        <div className="row">
-            <h2>{title}</h2>
-            <div className="row__posters">
-                {movies.map(movie => (
-                    <img src={`${baseUrl}${movie.poster_path}`} alt={movie.name}/>
-                ))}
-            </div>
-        </div>
-    )
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          console.log(movie?.name);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarger"}`}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
 }
 
-export default Row
+export default Row;
